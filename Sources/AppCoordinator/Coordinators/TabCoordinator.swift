ab255205ee02fd8597a7ab55b1c40fc9a382dfd6
Evgeny Kalashnikov <lumyk@me.com>
2020-03-10 16:42:42 +0200

    Added abbility to use generic RootType in TabCoordinator

--- Sources/AppCoordinator/Coordinators/TabCoordinator.swift
+++ Sources/AppCoordinator/Coordinators/TabCoordinator.swift
@@ -7,9 +7,15 @@
 
 import UIKit
 
+public protocol TabCoordinatorRootType: AnyObject {}
+
+extension UIViewController: TabCoordinatorRootType {}
+extension UINavigationController: TabCoordinatorRootType {}
+extension UISplitViewController: TabCoordinatorRootType {}
+
 public protocol TabCoordinator: Coordinator {
     associatedtype VC: UIViewController & Coordinated
-    associatedtype RootType: UIViewController
+    associatedtype RootType: TabCoordinatorRootType
     
     var storyboard: UIStoryboard { get }
     var identifier: String { get }
@@ -31,72 +37,85 @@ public extension TabCoordinator {
     func configuration(rootViewController: RootType) throws { /* optional */ }
 }
 
-public extension TabCoordinator where RootType: UIViewController {
-    
+public extension TabCoordinator {
+
     func start() throws {
+
+        if RootType.self == UIViewController.self {
+            try self.startViewController()
+        } else if RootType.self == UISplitViewController.self {
+            try self.startSplitViewController()
+        } else if RootType.self == UINavigationController.self {
+            try self.startNavigationController()
+        } else {
+            CoordinatorError.start(message: "Unknown RootType")
+        }
+    }
+
+    func startViewController() throws {
         guard var viewController = self.storyboard.instantiateViewController(withIdentifier: self.identifier) as? VC else {
             throw CoordinatorError.start(message: "Not correct viewController type for identifier '\(self.identifier)', must be '\(VC.self)'")
         }
-        
+
         viewController.setCoordinator(self)
-        
+
         try self.configuration(viewController: viewController)
         self.setViewController(viewController: viewController)
     }
-    
-    func configuration(rootViewController: UIViewController) throws { /* not need */ }
-    func setRootViewController(rootViewController: UIViewController) { /* not need */ }
-}
 
-public extension TabCoordinator where RootType == UINavigationController {
-    
-    func start() throws {
-        
+    func startNavigationController() throws {
+
         guard let navigation = self.storyboard.instantiateViewController(withIdentifier: self.identifier) as? UINavigationController else {
             throw CoordinatorError.start(message: "Not correct NavigationController for identifier '\(self.identifier)'")
         }
-        
+
         guard var viewController = navigation.topViewController as? VC else {
             throw CoordinatorError.start(message: "Not correct viewController type for identifier '\(self.identifier)', must be '\(VC.self)'")
         }
-        
+
         viewController.setCoordinator(self)
-        
+
         try self.configuration(viewController: viewController)
         self.setViewController(viewController: viewController)
-        
-        try self.configuration(rootViewController: navigation)
-        self.setRootViewController(rootViewController: navigation)
-    }
-}
 
-public extension TabCoordinator where RootType == UISplitViewController {
-    
-    func start() throws {
+
+        guard let _navigation = navigation as? RootType else {
+            throw CoordinatorError.start(message: "Not correct RootType for identifier '\(self.identifier)'")
+        }
+
+        try self.configuration(rootViewController: _navigation)
+        self.setRootViewController(rootViewController: _navigation)
+    }
+
+    func startSplitViewController() throws {
         guard let split = self.storyboard.instantiateViewController(withIdentifier: self.identifier) as? UISplitViewController else {
             throw CoordinatorError.start(message: "Not correct SplitViewController for identifier '\(self.identifier)'")
         }
-        
+
         if let navigation = split.viewControllers.first as? UINavigationController {
             guard var viewController = navigation.topViewController as? VC else {
                 throw CoordinatorError.start(message: "Not correct viewController type for identifier '\(self.identifier)', must be '\(VC.self)'")
             }
-            
+
             viewController.setCoordinator(self)
-            
+
             try self.configuration(viewController: viewController)
             self.setViewController(viewController: viewController)
-            
+
         } else if var viewController = split.viewControllers.first as? VC {
             viewController.setCoordinator(self)
-            
+
             try self.configuration(viewController: viewController)
             self.setViewController(viewController: viewController)
         } else {
             throw CoordinatorError.start(message: "Not correct viewController type for identifier '\(self.identifier)', must be '\(VC.self)'")
         }
-        
-        try self.configuration(rootViewController: split)
-        self.setRootViewController(rootViewController: split)
+
+        guard let _split = split as? RootType else {
+            throw CoordinatorError.start(message: "Not correct RootType for identifier '\(self.identifier)'")
+        }
+
+        try self.configuration(rootViewController: _split)
+        self.setRootViewController(rootViewController: _split)
     }
 }
